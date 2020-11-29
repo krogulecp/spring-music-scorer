53924da8488c67967f49219981a635abd96abf1b
Piotr Krogulec <piotr.krogulec@allegro.pl>
2020-11-29 09:40:07 +0100

    get album

--- src/main/java/pl/sda/spring/musicscorer/application/AlbumsRestController.java
+++ src/main/java/pl/sda/spring/musicscorer/application/AlbumsRestController.java
@@ -2,6 +2,7 @@ package pl.sda.spring.musicscorer.application;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,11 @@ public class AlbumsRestController {
 
     @GetMapping("/albums")
     public ResponseEntity<AlbumsResponse> getAlbums(){
+        return null;
+    }
+
+    @GetMapping("/albums/{id}")
+    public ResponseEntity<SingleAlbumResponse> getAlbum(@PathVariable String id){
         return null;
     }
 }
